d05385c1082c367fd26537462a1344202f46fb1d
Lotte Steenbrink <lotte@zombietetris.de>
2014-12-21 02:16:08 +0100

    fix aodvdiff.sh

--- aodvdiff.sh
+++ aodvdiff.sh
@@ -1,13 +1,11 @@
 # rfcdiff files between branches.
-function aodvdiff {
-	branch=$(git branch | grep "\* .*master")
-	if [[ $branch == *"master"* ]]; then
-		echo "Error: You're on the master branch. Please change to the branch that you want to compare against the master version (using git checkout) and run this command again."
-		return
-	fi
+branch=$(git branch | grep "\* .*master")
+if [[ $branch == *"master"* ]]; then
+	echo "Error: You're on the master branch. Please change to the branch that you want to compare against the master version (using git checkout) and run this command again."
+else
 	git show master:./$1 > master.txt
 	echo "executing: rfcdiff master.txt "$1
 	rfcdiff master.txt $1
 	rm master.txt
 	echo "done."
-}
+fi
